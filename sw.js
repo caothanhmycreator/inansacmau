17145f96a13ae78e467bfc164a0633b0871ff51a
Cao Thanh My <caothanhmy@gmail.com>
2026-04-15 02:08:44 +0700

    aaaa

--- sw.js
+++ sw.js
@@ -1,7 +1,4 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
 
-// ... Phía dưới này giữ nguyên các hàm cũ của Mỹ ...
-// sw.js - Service Worker cho Sắc Màu
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Đã cài đặt');
 });
